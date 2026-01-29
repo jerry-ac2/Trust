c7facd2874ca561ee417bc9ccee4238d4c33b664
Jeremiah <jeremiahegemonye12@gmail.com>
2026-01-29 08:34:49 +0100

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import HomePage from "./pages/homepage";
-import Welcome from "./pages/welcome";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/welcome" element={<Welcome />} />
     </Routes>
   );
 }
